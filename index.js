384d9df557874d3a234539a25ed90a7716be28fb
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2021-12-07 10:59:42 +0700

    feat: feature to show page info

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import { createServer } from "http";
 
 //create a server object:
 createServer(async function (req, res) {
-  console.log("Reequest ke ", req.url, " dimulai");
+  console.log(`Reequest start from ${req.url}`);
   res.setHeader("Content-Type", "text/html");
-    res.write("<br />Sample<br />"); //write a response to the client
+    res.write("<br />Sample page<br />"); //write a response to the client
     res.end(); //end the response
 }).listen(3000); //the server object listens on port 8080
