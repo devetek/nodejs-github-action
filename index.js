dc42183028ecda04cffdc8bf32ebbfed45019456
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2021-12-07 13:59:07 +0700

    feat: test coverage for CI

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import { createServer } from "http";
+const { createServer } = require("http");
 
-//create a server object:
-createServer(async function (req, res) {
-  console.log(`Reequest start from ${req.url}`);
-  res.setHeader("Content-Type", "text/html");
-    res.write("<br />Sample sample page 1<br />"); //write a response to the client
-    res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
+class CustomServer {
+    startServer() {
+        createServer(async (req, res) => {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<br />Sample sample page 1<br />"); //write a response to the client
+      res.end(); //end the response
+            res.end();
+        }).listen(8080);
+    }
+};
+
+module.exports = { CustomServer };
+
