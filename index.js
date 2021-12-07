58fb82be012ddbdc7d3391c9e7ee65a8f0b91723
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2021-12-07 11:14:44 +0700

    feat: show page info

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import { createServer } from "http";
 createServer(async function (req, res) {
   console.log(`Reequest start from ${req.url}`);
   res.setHeader("Content-Type", "text/html");
-    res.write("<br />Sample page<br />"); //write a response to the client
+    res.write("<br />Sample sample page<br />"); //write a response to the client
     res.end(); //end the response
 }).listen(3000); //the server object listens on port 8080
