e432ecac65a9e52a7dc680ce6ed1fa9e00dbd2e1
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2021-12-07 14:46:22 +0700

    ci: build basic CI

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const { createServer } = require("http");
 class CustomServer {
     startServer() {
         createServer(async (req, res) => {
-        res.setHeader("Content-Type", "text/html");
-        res.write("<br />Sample sample page 1<br />"); //write a response to the client
-      res.end(); //end the response
-            res.end();
+            res.setHeader("Content-Type", "text/html");
+            res.write("<br />Sample sample page 1<br />"); //write a response to the client
+            res.end(); //end the response
         }).listen(8080);
     }
 };
