66e59210ea25ab13f35349607c9c88ca4c675013
Amrih Prakasa <nedya.prakasa@tokopedia.com>
2021-12-08 12:42:18 +0700

    feat: home page response body v0.0.2 (#17)
    Breaking Changes - Please carefully to use this version 2
    
    BREAKING CHANGE: This body will be have different content, test should be failed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class CustomServer {
     startServer() {
         createServer(async (req, res) => {
             res.setHeader("Content-Type", "text/html");
-            res.write("<br />Sample sample page 1<br />"); //write a response to the client
+            res.write("<br />Sample sample page v0.0.2<br />"); //write a response to the client
             res.end(); //end the response
         }).listen(8080);
     }
